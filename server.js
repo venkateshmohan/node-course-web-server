30cc232f53dfe8a3066dc0979aa6288fa362cb92
venkateshmohan <venkytm18@gmail.com>
2018-06-14 00:00:07 -0500

    Project page added

--- server.js
+++ server.js
@@ -49,6 +49,11 @@ app.get('/about',(req,res)=>{
       currentDate: new Date().getDate()
     });
 });
+app.get('/projects',(req,res)=>{
+    res.render('projects.hbs',{
+         pageTitle:'Projects'
+    })
+})
 app.get('/bad',(req,res)=>{
     res.send({errorMessage:'Unable to handle request'
 });
